a2d2857a6c9e8bff926ad4d0fb8a7921e228c8c5
unknown <mauri.08.mp@gmail.com>
2023-11-07 21:41:50 -0300

    agregando mercadoliebre

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.use("/static", express.static(__dirname + "/public"));
+
+app.get("/home", (req,res) => {
+    res.sendFile(__dirname + "/views/home.html");
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.listen(PORT, () => {
+    console.log("Servidor Iniciado", PORT)
+});
+
